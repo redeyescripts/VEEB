0fefcbfa871f133dcdcfe6f853ce86d6c29dbc23
Opilane TTHK <opilane@tthk.ee>
2020-11-11 14:33:36 +0200

    first commit

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+const currentImage = document.querySelector('#image');
+const images = ['img/pic1.png','img/pic2.png','img/pic3.png'];
+
+
 function changeImage(){
-    console.log("Hello")}
+    console.log("Whats uppp");
+    let rnd = Math.floor(Math.random()* images.length);
+    console.log(rnd);
+}
+
